9a27587be4e57b814654fa2b1ca951e32e3c50f1
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-07 00:54:58 +0700

    Fix name on v0.0.2 (#7)

--- test/function.tf
+++ test/function.tf
@@ -1,5 +1,4 @@
 module "azure-function-test" {
   source  = "app.terraform.io/tribv-tfc-org/azure-function-test/azurerm"
-  version = "0.0.1"
-  ### change change change CHANGE
+  version = "0.0.2"
 }
