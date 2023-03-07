bb1f5d7999e500edf401a0a83d0f18b546971097
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-07 10:24:07 +0000

    Add Windows VM

--- test/2.vm.tf
+++ test/2.vm.tf
@@ -3,3 +3,8 @@ module "linux-vm" {
   version = "0.0.6"
   # v0.0.6 Fix nsg and update
 }
+
+module "windows-vm" {
+  source  = "app.terraform.io/tribv-tfc-org/windows-vm/azurerm"
+  version = "0.0.1"
+}
