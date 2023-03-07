da6363ee4212001450b7cd4f81acf11b62ac93e7
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-07 07:32:56 +0000

    Add bastion host

--- test/1.infra.tf
+++ test/1.infra.tf
@@ -1,8 +1,9 @@
 module "landingzone-infra" {
   source  = "app.terraform.io/tribv-tfc-org/landingzone-infra/azurerm"
-  version = "0.0.4"
+  version = "0.0.5"
   # v0.0.1
   # v0.0.2
   # v0.0.3 Add KV Policy for Ethan
   # v0.0.4 Medium update
+  # v0.0.5 Add bastion host
 }
