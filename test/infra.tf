fd158d50c774666c627b1096202208a83a8fc294
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-07 04:54:01 +0000

    Update

--- test/infra.tf
+++ test/infra.tf
@@ -1,7 +1,6 @@
 module "landingzone-infra" {
   source  = "app.terraform.io/tribv-tfc-org/landingzone-infra/azurerm"
   version = "0.0.3"
-  
   # v0.0.1
   # v0.0.2
   # v0.0.3 Add KV Policy for Ethan
