5de710d72d89bd990a22db06db97764282f6d0ef
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-07 02:01:29 +0000

    Add func code (sa)

--- test/function.tf
+++ test/function.tf
@@ -1,4 +1,11 @@
-# module "azure-function-test" {
-#   source  = "app.terraform.io/tribv-tfc-org/azure-function-test/azurerm"
-#   version = "0.0.2"
-# }
+module "azure-function-test" {
+  source  = "app.terraform.io/tribv-tfc-org/azure-function-test/azurerm"
+  version = "0.0.3"
+
+  infra_resource_group_name = "rg-infrarg-lz-2023"
+  vnet_name = "ethan-vnet"
+  subnet_name = "ethan-sbn01"
+  keyvault_name = "ethan-kv-adm"
+
+  storage_account_name = "ethanstalab01"
+}
