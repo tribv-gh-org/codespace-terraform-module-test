ba926f2f28973b876c5d08f0dc10e55fba4dc316
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-07 02:05:30 +0000

    Update rg app

--- test/function.tf
+++ test/function.tf
@@ -1,8 +1,14 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = "rg-func-app-2023"
+  location = "West Europe"
+}
+
 module "azure-function-test" {
   source  = "app.terraform.io/tribv-tfc-org/azure-function-test/azurerm"
   version = "0.0.3"
 
   infra_resource_group_name = "rg-infrarg-lz-2023"
+  resource_group_name       = azurerm_resource_group.resource_group.name
   vnet_name                 = "ethan-vnet"
   subnet_name               = "ethan-sbn01"
   keyvault_name             = "ethan-kv-adm"
