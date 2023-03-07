bb562a5c2fada9a19295d7f9bc78d7fb0d32dc20
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-07 14:06:17 +0700

    Fix code Infra (#18)

--- test/infra.tf
+++ test/infra.tf
@@ -1,7 +1,8 @@
 module "landingzone-infra" {
   source  = "app.terraform.io/tribv-tfc-org/landingzone-infra/azurerm"
-  version = "0.0.3"
+  version = "0.0.4"
   # v0.0.1
   # v0.0.2
   # v0.0.3 Add KV Policy for Ethan
+  # v0.0.4 Medium update
 }
