f92a95dd7b4f4f0ae7866f8308d78b0ccb061a9f
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-07 02:03:49 +0000

    Update format

--- test/function.tf
+++ test/function.tf
@@ -3,9 +3,9 @@ module "azure-function-test" {
   version = "0.0.3"
 
   infra_resource_group_name = "rg-infrarg-lz-2023"
-  vnet_name = "ethan-vnet"
-  subnet_name = "ethan-sbn01"
-  keyvault_name = "ethan-kv-adm"
+  vnet_name                 = "ethan-vnet"
+  subnet_name               = "ethan-sbn01"
+  keyvault_name             = "ethan-kv-adm"
 
   storage_account_name = "ethanstalab01"
 }
