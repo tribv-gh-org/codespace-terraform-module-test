73ed111ed6ee4927b17f54f4250538eefeea306e
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-07 07:35:24 +0000

    fix subnet

--- test/1.infra.tf
+++ test/1.infra.tf
@@ -1,9 +1,10 @@
 module "landingzone-infra" {
   source  = "app.terraform.io/tribv-tfc-org/landingzone-infra/azurerm"
-  version = "0.0.5"
+  version = "0.0.6"
   # v0.0.1
   # v0.0.2
   # v0.0.3 Add KV Policy for Ethan
   # v0.0.4 Medium update
   # v0.0.5 Add bastion host
+  # v0.0.6 Fix bastion host Subnet
 }
