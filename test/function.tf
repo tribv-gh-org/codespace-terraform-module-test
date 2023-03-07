b2918ede401e96c8f1f1d31c3ac3de44c14eb508
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-07 02:41:39 +0000

    Update code

--- test/function.tf
+++ test/function.tf
@@ -1,6 +1,6 @@
 module "azure-function-test" {
   source  = "app.terraform.io/tribv-tfc-org/azure-function-test/azurerm"
-  version = "0.0.3"
+  version = "0.0.4"
 
   infra_resource_group_name = "rg-infrarg-lz-2023"
   resource_group_name       = "rg-func-app-2023"
@@ -9,4 +9,8 @@ module "azure-function-test" {
   keyvault_name             = "ethan-kv-adm"
 
   storage_account_name = "ethanstalab01"
+
+  application_insights_name            = "ethan-func-ai"
+  inbound_private_endpoint_subnet_name = "ethan-sbn02"
+  function_app_name                    = "ethan-func-app-test"
 }
