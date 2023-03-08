b0d08f38b6c3d9b173fcbef136fde630f1fdd4ff
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-08 13:39:12 +0700

    Test the features

--- test/4.storageAccount.tf
+++ test/4.storageAccount.tf
@@ -1,6 +1,6 @@
 module "storage-account" {
   source  = "app.terraform.io/tribv-tfc-org/storage-account/azurerm"
-  version = "0.0.1"
+  version = "0.0.2"
   # insert required variables here
 
   infra_resource_group_name = "rg-infrarg-lz-2023"
@@ -10,11 +10,12 @@ module "storage-account" {
   action_group_name         = "ethanag"
   action_group_present      = false
 
-  resource_group_name                 = "rg-ethan-vm-2023"
-  location                            = "West Europe"
-  storage_account_name                = "stethantestdev12345678"
-  blob_container_names                = ["dev"]
-  blob_container_public_access_levels = ["blob", "container", "private"]
+  resource_group_name  = "rg-ethan-vm-2023"
+  location             = "West Europe"
+  storage_account_name = "stethantestdev12345678"
+  queue_name           = "ethantestqueue"
+  table_name           = "ethantesttable"
+  file_share_name      = "ethantestfileshare"
 
   application_tagvalue = "ethantest"
 }
