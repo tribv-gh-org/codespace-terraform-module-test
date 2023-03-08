f0e9a6ba3a90cc8b16ba77e0d750f58f94d77528
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-08 17:08:56 +0700

    Build FuncApp

---
+++ test/4.func_app.tf
@@ -0,0 +1,17 @@
+module "azure-function-test" {
+  source  = "app.terraform.io/tribv-tfc-org/azure-function-test/azurerm"
+  version = "0.1.1"
+  # insert required variables here
+
+  infra_resource_group_name            = "rg-infrarg-lz-2023"
+  resource_group_name                  = "rg-func-app-2023"
+  vnet_name                            = "ethan-vnet"
+  vnet_integration_subnet_name         = "ethan-sbn01"
+  inbound_private_endpoint_subnet_name = "ethan-sbn02"
+  functions_storage_account_name       = "stethantestdev12345678"
+  functions_storage_account_rg_name    = "rg-ethan-vm-2023"
+  hosting_plan_name                    = "plan-ethantest-weu-redundant"
+  keyvault_name                        = "ethan-kv-adm"
+  application_insights_name            = "ethan-func-ai"
+  function_app_name                    = "ethan-func-app-test"
+}
