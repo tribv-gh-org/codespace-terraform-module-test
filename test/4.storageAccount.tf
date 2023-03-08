00bef235fe28268e7a5cd1a973310181cc9cfe36
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-08 12:52:45 +0700

    Build SA

---
+++ test/4.storageAccount.tf
@@ -0,0 +1,21 @@
+module "storage-account" {
+  source  = "app.terraform.io/tribv-tfc-org/storage-account/azurerm"
+  version = "0.0.1"
+  # insert required variables here
+
+  infra_resource_group_name           = "rg-infrarg-lz-2023"
+  vnet_name                           = "ethan-vnet"
+  subnet_name                         = "ethan-sbn01"
+  resource_group_name                 = "rg-ethan-vm-2023"
+  location                            = "West Europe"
+  storage_account_name                = "stadvautotestdev12345678"
+  blob_container_names                = ["dev"]
+  blob_container_public_access_levels = ["blob", "container", "private"]
+  # queue_name           = "advautotestqueue"
+  # table_name           = "advautotesttable"
+  # file_share_name      = "advautotestfileshare"
+  keyvault_name        = "ethan-kv-adm"
+  action_group_name    = "ethanag"
+  action_group_present = false
+  application_tagvalue = "advautotest"
+}
