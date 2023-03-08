0f2f8b38cf244a59d60a50de5711bdbbeefae536
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-08 14:49:15 +0700

    Destroy SA (#39)

--- test/4.storageAccount.tf
+++ test/4.storageAccount.tf
@@ -1,23 +1,23 @@
-module "storage-account" {
-  source  = "app.terraform.io/tribv-tfc-org/storage-account/azurerm"
-  version = "0.0.3"
-  # insert required variables here
+# module "storage-account" {
+#   source  = "app.terraform.io/tribv-tfc-org/storage-account/azurerm"
+#   version = "0.0.3"
+#   # insert required variables here
 
-  infra_resource_group_name = "rg-infrarg-lz-2023"
-  vnet_name                 = "ethan-vnet"
-  subnet_name               = "ethan-sbn01"
-  keyvault_name             = "ethan-kv-adm"
-  action_group_name         = "ethanag"
-  action_group_present      = false
+#   infra_resource_group_name = "rg-infrarg-lz-2023"
+#   vnet_name                 = "ethan-vnet"
+#   subnet_name               = "ethan-sbn01"
+#   keyvault_name             = "ethan-kv-adm"
+#   action_group_name         = "ethanag"
+#   action_group_present      = false
 
-  resource_group_name                 = "rg-ethan-vm-2023"
-  location                            = "West Europe"
-  storage_account_name                = "stethantestdev12345678"
-  blob_container_names                = ["dev"]
-  blob_container_public_access_levels = ["blob", "container", "private"]
-  queue_name                          = "ethantestqueue"
-  table_name                          = "ethantesttable"
-  file_share_name                     = "ethantestfileshare"
+#   resource_group_name                 = "rg-ethan-vm-2023"
+#   location                            = "West Europe"
+#   storage_account_name                = "stethantestdev12345678"
+#   blob_container_names                = ["dev"]
+#   blob_container_public_access_levels = ["blob", "container", "private"]
+#   queue_name                          = "ethantestqueue"
+#   table_name                          = "ethantesttable"
+#   file_share_name                     = "ethantestfileshare"
 
-  application_tagvalue = "ethantest"
-}
+#   application_tagvalue = "ethantest"
+# }
