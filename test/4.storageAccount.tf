a9e9c6d42833e7bda7c49081124797cc6d7e1fef
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-08 15:08:12 +0700

    Feat/func (#40)
    * Destroy SA
    
    * Provision SA

--- test/4.storageAccount.tf
+++ test/4.storageAccount.tf
@@ -1,23 +1,22 @@
-# module "storage-account" {
-#   source  = "app.terraform.io/tribv-tfc-org/storage-account/azurerm"
-#   version = "0.0.3"
-#   # insert required variables here
+module "storage-account" {
+  source  = "app.terraform.io/tribv-tfc-org/storage-account/azurerm"
+  version = "0.0.4"
+  # insert required variables here
 
-#   infra_resource_group_name = "rg-infrarg-lz-2023"
-#   vnet_name                 = "ethan-vnet"
-#   subnet_name               = "ethan-sbn01"
-#   keyvault_name             = "ethan-kv-adm"
-#   action_group_name         = "ethanag"
-#   action_group_present      = false
+  infra_resource_group_name = "rg-infrarg-lz-2023"
+  vnet_name                 = "ethan-vnet"
+  subnet_name               = "ethan-sbn01"
+  keyvault_name             = "ethan-kv-adm"
+  action_group_name         = "ethanag"
+  action_group_present      = false
+  application_tagvalue      = "ethantest"
 
-#   resource_group_name                 = "rg-ethan-vm-2023"
-#   location                            = "West Europe"
-#   storage_account_name                = "stethantestdev12345678"
-#   blob_container_names                = ["dev"]
-#   blob_container_public_access_levels = ["blob", "container", "private"]
-#   queue_name                          = "ethantestqueue"
-#   table_name                          = "ethantesttable"
-#   file_share_name                     = "ethantestfileshare"
-
-#   application_tagvalue = "ethantest"
-# }
+  resource_group_name                 = "rg-ethan-vm-2023"
+  location                            = "West Europe"
+  storage_account_name                = "stethantestdev12345678"
+  blob_container_names                = ["dev_blob"]
+  blob_container_public_access_levels = ["blob", "container", "private"]
+  queue_names                         = ["dev_queue"]
+  table_names                         = ["dev_table"]
+  share_names                         = ["dev_share_file"]
+}
