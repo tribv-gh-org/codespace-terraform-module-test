eae54b8e49f6110e6945d94662a6d8d6fb702f59
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-08 13:18:25 +0700

    Update format code

--- test/4.storageAccount.tf
+++ test/4.storageAccount.tf
@@ -3,19 +3,14 @@ module "storage-account" {
   version = "0.0.1"
   # insert required variables here
 
-  infra_resource_group_name           = "rg-infrarg-lz-2023"
-  vnet_name                           = "ethan-vnet"
-  subnet_name                         = "ethan-sbn01"
-  resource_group_name                 = "rg-ethan-vm-2023"
-  location                            = "West Europe"
-  storage_account_name                = "stethantestdev12345678"
-  # blob_container_names                = ["dev"]
-  # blob_container_public_access_levels = ["blob", "container", "private"]
-  # queue_name           = "ethantestqueue"
-  # table_name           = "ethantesttable"
-  # file_share_name      = "ethantestfileshare"
-  keyvault_name        = "ethan-kv-adm"
-  action_group_name    = "ethanag"
-  action_group_present = false
-  application_tagvalue = "ethantest"
+  infra_resource_group_name = "rg-infrarg-lz-2023"
+  vnet_name                 = "ethan-vnet"
+  subnet_name               = "ethan-sbn01"
+  resource_group_name       = "rg-ethan-vm-2023"
+  location                  = "West Europe"
+  storage_account_name      = "stethantestdev12345678"
+  keyvault_name             = "ethan-kv-adm"
+  action_group_name         = "ethanag"
+  action_group_present      = false
+  application_tagvalue      = "ethantest"
 }
