2a575ebcd7561ef46e3c580e58509d0e102b8b09
buivantribv <124042557+buivantribv@users.noreply.github.com>
2023-03-08 19:16:41 +0700

    Update subnet

--- test/2.vm.tf
+++ test/2.vm.tf
@@ -1,5 +1,5 @@
 module "windows-vm" {
   source  = "app.terraform.io/tribv-tfc-org/windows-vm/azurerm"
-  version = "0.0.5"
-  # v0.0.5 Add new NIC
+  version = "0.0.6"
+  # v0.0.5 v0.0.6 Add new NIC
 }
